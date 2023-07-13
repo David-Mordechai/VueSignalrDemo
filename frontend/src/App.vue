b570bae67f0d39dc345b474a83cf24035d4ab6c9
David Mordechai <David.mordechai79@gmail.com>
2023-07-13 16:24:03 +0300

    implemented reliable connection

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,73 @@
 <script setup lang="ts">
-import { ref, onUnmounted } from 'vue';
-import SignalrService from './signalrService'
-
-const socket = new SignalrService("http://localhost:40001/ws/").start();
+import { ref, onUnmounted, onMounted } from 'vue';
+import CommunicationService from './CommunicationService'
 
 const command1BtnColor = ref('');
 const command2BtnColor = ref('');
+const isSocketConnected = ref<boolean>(false);
+let socket: CommunicationService;
+
+onMounted(async () => {
+  socket = new CommunicationService("http://localhost:40001/ws/");
+
+  socket.ConnectionState = (connected: boolean) => {
+    isSocketConnected.value = connected;
+  }
+
+  socket.ReceiveReport = (color: string, command: string) => {
+    switch (command) {
+      case 'command1':
+        command1BtnColor.value = color;
+        break;
+      case 'command2':
+        command2BtnColor.value = color;
+        break;
+      default:
+        break;
+    }
+  }
+
+  await socket.start();
+});
+
+onUnmounted(async () => {
+  await socket.stop();
+})
 
 function sendCommand(command: string) {
   socket.SendCommand(command);
 }
-
-socket.ReceiveReport = (color: string, command: string) => {
-  switch (command) {
-    case 'command1':
-      command1BtnColor.value = color;
-      break;
-    case 'command2':
-      command2BtnColor.value = color;
-      break;
-    default:
-      break;
-  }
-}
-
-onUnmounted(() => {
-  socket.stop();
-})
 </script>
 
 <template>
-  <div>
-    <button @click="sendCommand('command1')" :style="{ backgroundColor: command1BtnColor }">Command1</button>
-    <button @click="sendCommand('command2')" :style="{ backgroundColor: command2BtnColor }">Command2</button>
+  <div class="parent">
+    <div class="status" v-if="isSocketConnected === true" style="background-color: green;">Connected to server</div>
+    <div class="status" v-if="isSocketConnected === false" style="background-color: red;">Disconnected from server</div>
+    <button class="cmd1" @click="sendCommand('command1')" :style="{ backgroundColor: command1BtnColor }"
+      v-bind:disabled="!isSocketConnected">Command1</button>
+    <button class="cmd2" @click="sendCommand('command2')" :style="{ backgroundColor: command2BtnColor }"
+      v-bind:disabled="!isSocketConnected">Command2</button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.parent {
+  display: grid;
+  grid-gap: 5px;
+  grid-template-areas:
+    "status status "
+    "cmd1 cmd2";
+}
+
+.status {
+  grid-area: status;
+}
+
+.cmd1 {
+  grid-area: cmd1;
+}
+
+.cmd2 {
+  grid-area: cmd2;
+}
+</style>
---
+++ frontend/src/CommunicationService.ts
@@ -0,0 +1,60 @@
+import { HubConnectionBuilder, LogLevel, HubConnection, HubConnectionState, IRetryPolicy, RetryContext } from '@microsoft/signalr';
+export default class CommunicationService {
+
+    ReceiveReport: ((color: string, command: string) => void) | undefined;
+    ConnectionState: ((connected: boolean) => void) | undefined;
+
+    private connection: HubConnection;
+
+    constructor(url: string) {
+        this.connection = new HubConnectionBuilder()
+            .withUrl(url, { withCredentials: false })
+            .withAutomaticReconnect(new ReconnectRetryPolicy())
+            .configureLogging(LogLevel.Information)
+            .build();
+
+        this.connection.on("receiveReport", (color, command) => this.ReceiveReport?.call(this, color, command));
+        this.connection.onreconnecting(() => this.ConnectionState?.call(this, false));
+        this.connection.onreconnected(() => this.ConnectionState?.call(this, true));
+    }
+
+    async start() {
+        let timeoutId: number | undefined;
+        try {
+            await this.connection.start();
+            this.ConnectionState?.call(this, true);
+        } catch (error) {
+            console.log(`Signalr fail to connect. Attempt to reconnect after 1 minute`)
+            this.ConnectionState?.call (this, false);
+            timeoutId = setTimeout(async () =>  {
+                await this.start();
+            }, 1000 * 60);
+        }
+    }
+
+    async stop() {
+        if (this.connection.state !== HubConnectionState.Disconnected)
+            await this.connection.stop();
+        this.ConnectionState?.call(this, false);
+    }
+
+    SendCommand = (commandType: string) => this.connection.send('sendCommand', commandType);
+    
+}
+
+/** Configures signalr.HubConnection to automatically attempt to reconnect if the connection is lost.
+ *  ##### Retry count: 
+ * * count < 10 - interval 1s
+ * * count < 50 - interval 30s
+ * * count > 50 - interval 1m
+ */
+class ReconnectRetryPolicy implements IRetryPolicy {
+    nextRetryDelayInMilliseconds(retryContext: RetryContext): number | null {
+        var count = retryContext.previousRetryCount;
+        switch (true) {
+            case count < 10: return 1000;
+            case count < 50: return 1000 * 30;
+            default: return 1000 * 60;
+        };
+    }
+}
