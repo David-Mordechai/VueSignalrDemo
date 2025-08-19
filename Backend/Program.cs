51199e72d3d01229d938e62b9cf17f2b553bf509
David Mordechai <david.mordechai79@gmail.com>
2025-08-19 13:04:44 +0300

    Serve app ui (SPA) from the web api with signalr functionality

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,10 +1,12 @@
+using System.Reflection;
 using Backend;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
 builder.Services.AddSignalR();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +22,33 @@ app.MapControllers();
 
 app.MapHub<PayloadHub>("/ws");
 
-app.MapGet("test", () => "test");
+app.MapGet("api/test", () => "test api");
+
+var uiAssembly = Assembly.GetCallingAssembly();
+var uiProvider = new ManifestEmbeddedFileProvider(uiAssembly, "AppUi");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = uiProvider,
+    ContentTypeProvider = new FileExtensionContentTypeProvider()
+});
+
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path;
+
+    if(path.StartsWithSegments("/ws") || path.StartsWithSegments("/api"))
+    {
+        await next();
+        return;
+    }
+    
+    var index = uiProvider.GetFileInfo("index.html");
+    if (index.Exists)
+    {
+        context.Response.ContentType = "text/html; charset=utf-8";
+        await context.Response.SendFileAsync(index);
+    }
+});
 
 app.Run("http://*:40001/");
